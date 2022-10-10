89fe4fdb2c1bfe7fab3d618e1c5faa1fbac1fb5f
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2022-10-10 16:53:45 -0400

    Implemented jump move function

--- checkers.py
+++ checkers.py
@@ -1,24 +1,109 @@
+from copy import deepcopy
 from sys import argv
 from typing import *
 
 
-Grid = List[List[int]]
-Cord = Tuple[int, int]
+DIM = 8
+EMPTY = '.'
+MAX = 'r'
+MIN = 'b'
+
+
+Grid = List[List[str]]
+Coord = Tuple[int, int]
+
+# Assuming that our AI (red player) starts from bottom of board, and enemy AI (black player) starts from top
 
 
 class State:
 
     id: str
     grid: Grid
-    parent: Optional['State']
+    is_max_turn: bool
 
-    def __init__(self, grid: Grid, parent: Optional['State'] = None) -> None:
+    def __init__(self, grid: Grid, is_max_turn: bool) -> None:
         self.grid = grid
-        self.parent = parent
+        self.is_max_turn = is_max_turn
         self._generate_id()
 
     def get_successors(self) -> List['State']:
-        pass
+
+        if self.is_max_turn:
+            color = 'r'
+            move_direction = -1
+        else:
+            color = 'b'
+            move_direction = 1
+
+        for row in range(self.grid):
+            for col in range(self.grid[row]):
+
+                piece = self.grid[row][col]
+
+                if piece.lower() != color:
+                    continue
+                # if piece == color:
+                    # self.get_moves((row, col), move_direction, [1])
+                # King
+                # else:
+                    # self.get_moves((row, col), move_direction, [-1, 1])
+
+    def _jump_move(self, grid: Grid, pos: Coord, depth: int = 0) -> List[Grid]:
+
+        row, col = pos
+        piece_to_move = grid[row][col]
+        row_deltas = self._get_row_deltas(piece_to_move)
+
+        moves = []
+
+        for row_delta in row_deltas:
+            for col_delta in [-1, 1]:
+
+                n_row = row + row_delta
+                n_col = col + col_delta
+
+                if n_row not in range(DIM) or n_col not in range(DIM):
+                    continue
+                if grid[n_row][n_col] == EMPTY:
+                    continue
+                if grid[n_row][n_col] == piece_to_move:
+                    continue
+
+                # Jump opportunity
+                jump_row = pos[0] + (2 * row_delta)
+                jump_col = pos[1] + (2 * col_delta)
+
+                if jump_row not in range(DIM) or jump_col not in range(DIM):
+                    continue
+                if grid[jump_row][jump_col] != EMPTY:
+                    continue
+
+                # Jump can be made
+                new_move = self._swap_pieces(grid, pos, (jump_row, jump_col))
+                new_move[n_row][n_col] = EMPTY
+                moves += self._jump_move(new_move, (jump_row, jump_col), depth + 1)
+
+        if len(moves):
+            return moves
+        if depth > 0:
+            return [grid]
+        return []
+
+    def _get_row_deltas(self, piece: str) -> List[int]:
+        if piece == MAX:
+            return [-1]
+        elif piece == MIN:
+            return [1]
+        else:
+            return [-1, 1]
+
+    def _swap_pieces(self, grid: Grid, old_pos: Coord, new_pos: Coord) -> Grid:
+
+        copied_grid = deepcopy(grid)
+        copied_grid[new_pos[0]][new_pos[1]] = grid[old_pos[0]][old_pos[1]]
+        copied_grid[old_pos[0]][old_pos[1]] = EMPTY
+
+        return copied_grid
 
     def _generate_id(self) -> None:
         self.id = ""
@@ -27,7 +112,26 @@ class State:
                 self.id += col
 
 
-def generate_grid(filename: str)-> Grid:
+def basic_utility(state: State) -> int:
+
+    max_count = 0
+    min_count = 0
+
+    for row in state.grid:
+        for piece in row:
+            if piece.lower() == MAX:
+                max_count += 1
+                if piece.isupper():
+                    max_count += 1
+            elif piece.lower() == MIN:
+                min_count += 1
+                if piece.isupper():
+                    min_count += 1
+
+    return (max_count - min_count)
+
+
+def generate_grid(filename: str) -> Grid:
 
     grid = []
 
@@ -38,8 +142,9 @@ def generate_grid(filename: str)-> Grid:
     return grid
 
 
-def main(input_filename: str, output_filename:str) -> None:
-    board = generate_grid(input_filename)
+def main(input_filename: str, output_filename: str) -> None:
+    initial_grid = generate_grid(input_filename)
+    initial_state = State(initial_grid, True)
 
 
 if __name__ == "__main__":
--- test_helpers.py
+++ test_helpers.py
@@ -21,5 +21,58 @@ class TestGenerateGrid(unittest.TestCase):
         self.assertEqual(expected_grid, result)
 
 
+class TestBasicUtility(unittest.TestCase):
+    def test_some_max_and_min(self):
+        grid = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", "b", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "R"],
+            [".", ".", "b", ".", "b", ".", ".", "."],
+            [".", ".", ".", "b", ".", ".", ".", "r"],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "r", ".", ".", ".", "."],
+            [".", ".", ".", ".", "B", ".", ".", "."],
+        ]
+
+        state = State(grid, True)
+        utility = basic_utility(state)
+
+        self.assertEqual(-2, utility)
+    
+    def test_no_max_some_min(self):
+        grid = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", "b", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", "b", ".", "b", ".", ".", "."],
+            [".", ".", ".", "b", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", "B", ".", ".", "."],
+        ]
+
+        state = State(grid, True)
+        utility = basic_utility(state)
+
+        self.assertEqual(-6, utility)
+
+    def test_some_max_no_min(self):
+        grid = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", "r", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", "r", ".", "r", ".", ".", "."],
+            [".", ".", ".", "r", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", "R", ".", ".", "."],
+        ]
+
+        state = State(grid, True)
+        utility = basic_utility(state)
+
+        self.assertEqual(6, utility)
+
+
 if __name__ == "__main__":
     unittest.main()
