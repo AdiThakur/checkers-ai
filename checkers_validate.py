25f173b47220dc7cb2c3149b4be96939b1a303ee
Thakur Aditya <thakur15@teach.cs.toronto.edu>
2022-10-12 18:20:23 -0400

    Updated checkers_validate script to validate all provided samples

--- checkers_validate.py
+++ checkers_validate.py
@@ -2,23 +2,26 @@ import os
 
 if __name__ == '__main__':
 
-    version = '2'
-    print(f"Input file: input{version}.txt, output file: output{version}.txt")
-    os.system(f"python checkers.py ./checkers_validate/input{version}.txt ./checkers_validate/output{version}.txt")
-
-    output_read = open(f"./checkers_validate/output{version}.txt", "r")
-    solution_read = open(f"./checkers_validate/solution{version}.txt", "r")
-
-    output_lines = output_read.readlines()
-    solution_lines = solution_read.readlines()
     passed = True
 
-    for index in range(1, len(output_lines)):
-        if output_lines[index].strip() != solution_lines[index].strip():
-            print(f"Line {index + 1}: "
-                  f"Expected <{output_lines[index].strip()}> "
-                  f"Encountered <{solution_lines[index].strip()}>\n")
-            passed = False
-            break
+    for version in [0, 1, 2]:
 
-    print("Checkers output matches solution file.")
+        print(f"Input file: input{version}.txt, output file: output{version}.txt")
+        os.system(f"python3 checkers.py ./checkers_validate/input{version}.txt ./checkers_validate/output{version}.txt")
+
+        output_read = open(f"./checkers_validate/output{version}.txt", "r")
+        solution_read = open(f"./checkers_validate/solution{version}.txt", "r")
+
+        output_lines = output_read.readlines()
+        solution_lines = solution_read.readlines()
+
+        for index in range(1, len(output_lines)):
+            if output_lines[index].strip() != solution_lines[index].strip():
+                print(f"Line {index + 1}: "
+                      f"Expected <{output_lines[index].strip()}> "
+                      f"Encountered <{solution_lines[index].strip()}>\n")
+                passed = False
+                break
+
+    if passed:
+        print("Checkers output matches solution file.")
